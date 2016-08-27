4497e39f0942df092c7cf833273d13eb768f2bb6
Drake Ballard <drakeballard@gmail.com>
2016-08-27 15:29:57 -0500

    id association to crystals

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//Global VAR
+//--------------------------------------------------------
+
+//Crystals
+var crystals = {
+	blue:
+	{
+		name: "Blue",
+		value: 0 
+	},	
+	red:
+	{
+		name: "Red",
+		value: 0 
+	},
+	green:
+	{
+		name: "Green",
+		value: 0 
+	},
+	yellow:
+	{
+		name: "Yellow",
+		value: 0 
+	}
+};
+//Scores of the current and target
+
+var currentScore = 0;
+var targetScore = 0;
+
+//wins and losses
+
+var  winCount = 0;
+var lossCount = 0;
+
+//Funcions
+//--------------------------------------------------------
+
+//Main Process
+//-------------------------------------------------------
